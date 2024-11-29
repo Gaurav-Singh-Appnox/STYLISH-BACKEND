cc35ea6a7a71a9c40b1a65306e0d46d9e2dcfa07
Gaurav-Singh-Appnox <gaurav.singh@appnox.ai>
2024-11-29 14:35:59 +0530

    User model updated

--- src/models/User.js
+++ src/models/User.js
@@ -4,12 +4,12 @@ const userSchema = new mongoose.Schema(
   {
     firstName: {
       type: String,
-      required: true,
+
       trim: true,
     },
     lastName: {
       type: String,
-      required: true,
+
       trim: true,
     },
     email: {
